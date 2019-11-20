59ebd300c938d01bdaa76d578e23f5b858f1198d
Omar Rodriguez <otorres@growthaccelerationpartners.com>
2019-11-20 08:41:09 -0600

    Fixes and whastapp added

--- SecretSanta/Program.cs
+++ SecretSanta/Program.cs
@@ -3,26 +3,31 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Support.UI;
 
 namespace SecretSanta
 {
     class MainClass
     {
+        private static string sentBtn = "//span[@data-icon='send']";
         public static void Main(string[] args)
         {
             Console.WriteLine("Secret Santa!");
             //LoadJson();
-            SendMessage();
+            ConnectToWhatsApp("50688237198", "Secret Santa");
         }
 
 
-        public static void LoadJson()
+        public static List<Person> LoadJson()
         {
             using (StreamReader r = new StreamReader("../../Family.txt"))
             {
                 string json = r.ReadToEnd();
                 List<Person> family = GetFamilyPeople(json.Split(','));
-                SortSecretSanta(family);
+                return SortSecretSanta(family);
             }
         }
 
@@ -43,7 +48,7 @@ namespace SecretSanta
             return exclusionList.Split('-').ToList();
         }
 
-        private static void SortSecretSanta(List<Person> family)
+        private static List<Person> SortSecretSanta(List<Person> family)
         {
             foreach (Person person in family)
             {
@@ -57,6 +62,32 @@ namespace SecretSanta
                 person.Target = target.Name;
                 target.Targeted = true;
                 Console.WriteLine($"{person.Name} => {person.Target}");
+            }
+
+            return family;
+        }
+
+        private static void ConnectToWhatsApp(string phone, string msg)
+        {
+            IWebDriver driver;
+            var options = new ChromeOptions();
+            options.AddArguments(@"user-data-dir=/Users/orodriguez/Library/Application Support/Google/Chrome/");
+            driver = new ChromeDriver(options);
+            try
+            {
+                driver.Url = $"https://web.whatsapp.com/send?phone={phone}&text={msg}&source&data";
+
+                By locator = By.XPath(sentBtn);
+                WebDriverWait wait = new WebDriverWait(driver,
+                   System.TimeSpan.FromSeconds(15));
+                var button = wait.Until(d => 
+                           d.FindElement(locator));
+                wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(By.XPath(sentBtn)));
+                button.Click();
+            }
+            finally
+            {
+                driver.Close();
             }
         }
 
@@ -87,8 +118,6 @@ namespace SecretSanta
             {
                 Console.WriteLine(se.Message);
             }
-            Console.ReadLine();
-
         }
     }
 }
